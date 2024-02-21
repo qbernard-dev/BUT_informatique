1c3404ffae3d8593b10b7767b4bd8e91e5a2775c
qbernard-dev <quentinbernard49770@gmail.com>
2024-02-21 08:41:35 +0100

    new cours js

--- JavaScript/exo_rectangle.js
+++ JavaScript/exo_rectangle.js
@@ -47,3 +47,57 @@ let r2 = new Rectangle(50,50);
 console.log(`Est-ce un carré ? ${r2.estCarre() ? 'Oui' : 'Non'}`);
 
 console.log(Rectangle.comparerRectangle(r1, r2));
+
+class Carre extends Rectangle{
+  constructor(long){
+    super(long);
+  }
+
+  calcPerimetre(){
+  	return `le périmètre est de ${this._longueur*4}`
+  }
+
+  estCarre(){
+    return true;
+  }
+
+  get aire(){
+    return this._longueur * this._longueur;
+  }
+
+  toString(){
+    return `Carré de ${this._longueur} cm de côté`
+  }
+}
+
+let c1 = new Carre(30);
+console.log(c1.toString());
+console.log(c1.calcPerimetre()); 
+console.log(`L'aire est de ${c1.aire} cm²`); 
+console.log(`Est-ce un carré ? ${c1.estCarre() ? 'Oui' : 'Non'}`);
+console.log(Rectangle.comparerRectangle(r1, c1));
+
+
+const jsonData = `[
+              { "dimensions": { "valeur1": 10, "valeur2": 20 } },
+              { "dimensions": { "valeur1": 17, "valeur2": 17 } },
+              { "dimensions": { "valeur1": 5, "valeur2": 8 } },
+              { "dimensions": { "valeur1": 12, "valeur2": 12 } },
+              { "dimensions": { "valeur1": 5, "valeur2": 5 } },
+              { "dimensions": { "valeur1": 18, "valeur2": 25 } },
+              { "dimensions": { "valeur1": 10, "valeur2": 22 } },
+              { "dimensions": { "valeur1": 15, "valeur2": 15 } },
+              { "dimensions": { "valeur1": 5, "valeur2": 8 } },
+              { "dimensions": { "valeur1": 11, "valeur2": 12 } },
+              { "dimensions": { "valeur1": 19, "valeur2": 19 } },
+              { "dimensions": { "valeur1": 2, "valeur2": 2 } },
+              { "dimensions": { "valeur1": 17, "valeur2": 22 } }
+            ]`;
+          
+jsonData.map((element) => {
+  if(element[1] == element[2]){
+    new Rectangle(element[1], element[2]);
+  } else {
+    new Carre(element[1]);
+  } 
+})
